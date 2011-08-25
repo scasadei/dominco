e323acd7279a217c02d67e852e0eaacfeecd36fd
stefano casadei <scasadei@yahoo.com>
2011-08-25 19:43:35 +0200

    SWTEditor lists two Hellos vertically

--- core/src/net/efano/dominco/ui/SWTEditor.java
+++ core/src/net/efano/dominco/ui/SWTEditor.java
@@ -1,6 +1,9 @@
 package net.efano.dominco.ui;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.*;
 
 public class SWTEditor implements IEditor {
@@ -8,8 +11,18 @@ public class SWTEditor implements IEditor {
 	   public static void main (String [] args) {
 		      Display display = new Display ();
 		      Shell shell = new Shell (display);
-		      Label label = new Label (shell, SWT.CENTER);
-		      label.setText ("Hello_world");
+		      
+				
+				shell.setLayout(new RowLayout(SWT.VERTICAL));
+		      
+		      
+			      Label label = new Label (shell, SWT.CENTER);
+			      label.setText ("Hello_world");
+			      
+			      Label label2 = new Label (shell, SWT.CENTER);
+			      label2.setText ("Hello_world2");
+
+			      
 		      label.setBounds (shell.getClientArea ());
 		      shell.open ();
 		      while (!shell.isDisposed ()) {
