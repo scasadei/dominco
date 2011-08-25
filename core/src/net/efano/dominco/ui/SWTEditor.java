e7cfbdabda0fad90eab815ce1cd45e288fa57d8a
stefano casadei <scasadei@yahoo.com>
2011-08-25 20:27:46 +0200

    SWTEditor now has TabFolder snippet: http://git.eclipse.org/c/platform/eclipse.platform.swt.git/tree/examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet76.java which according to http://www.java2s.com/Tutorial/Java/0280__SWT/TabFolderTabItemvsCTabFolderCTabItem.htm is less versatible than CTabFolder;
    Added an executable ShowCTabFolder found in
    http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/DemonstratesCTabFolder.htm;

--- core/src/net/efano/dominco/ui/SWTEditor.java
+++ core/src/net/efano/dominco/ui/SWTEditor.java
@@ -1,12 +1,7 @@
 package net.efano.dominco.ui;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CTabFolder;
-import org.eclipse.swt.custom.CTabFolder2Adapter;
-import org.eclipse.swt.custom.CTabFolderEvent;
-import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.layout.FillLayout;
-
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.*;
 
 public class SWTEditor implements IEditor {
@@ -14,40 +9,23 @@ public class SWTEditor implements IEditor {
 	   public static void main (String [] args) {
 		      Display display = new Display ();
 		      Shell shell = new Shell (display);
+
 		      
-		  	shell.setLayout(new FillLayout());
-			CTabFolder folder = new CTabFolder(shell, SWT.BORDER);
-			for (int i = 0; i < 4; i++) {
-				CTabItem item = new CTabItem(folder, SWT.CLOSE);
-				item.setText("Item "+i);
-				Text text = new Text(folder, SWT.MULTI);
-				text.setText("Content for Item "+i);
-				item.setControl(text);
+		  	final TabFolder tabFolder = new TabFolder (shell, SWT.BORDER);
+			Rectangle clientArea = shell.getClientArea ();
+			tabFolder.setLocation (clientArea.x, clientArea.y);
+			for (int i=0; i<6; i++) {
+				TabItem item = new TabItem (tabFolder, SWT.NONE);
+				item.setText ("TabItem " + i);
+				Button button = new Button (tabFolder, SWT.PUSH);
+				button.setText ("Page " + i);
+				item.setControl (button);
 			}
+			tabFolder.pack ();
+			shell.pack ();
+			shell.open ();		      
+		      
 			
-			final CTabItem specialItem = new CTabItem(folder, SWT.CLOSE);
-			specialItem.setText("Don't Close Me");
-			Text text = new Text(folder, SWT.MULTI);
-			text.setText("This tab can never be closed");
-			specialItem.setControl(text);
-				
-			folder.addCTabFolder2Listener(new CTabFolder2Adapter() {
-				public void close(CTabFolderEvent event) {
-					if (event.item.equals(specialItem)) {
-						event.doit = false;
-					}
-				}
-			});
-			
-			final CTabItem noCloseItem = new CTabItem(folder, SWT.NONE);
-			noCloseItem.setText("No Close Button");
-			Text text2 = new Text(folder, SWT.MULTI);
-			text2.setText("This tab does not have a close button");
-			noCloseItem.setControl(text2);
-			
-			shell.pack();
-			shell.open();
-		      shell.open ();
 		      while (!shell.isDisposed ()) {
 		         if (!display.readAndDispatch ()) display.sleep ();
 		      }
