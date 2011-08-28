425b7bf01236fb8a060fcf84ea7932c2dceeb6e2
stefano casadei <scasadei@yahoo.com>
2011-08-28 16:53:16 +0200

    inserted ScrolledComposite; the scrolls bars dont showup

--- core/src/net/efano/dominco/ui/SWTEditor.java
+++ core/src/net/efano/dominco/ui/SWTEditor.java
@@ -1,6 +1,7 @@
 package net.efano.dominco.ui;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -23,20 +24,34 @@ public class SWTEditor implements IEditor {
 
 	      
 	  	final TabFolder tabFolder = new TabFolder (rootWidget, SWT.BORDER);
-		Rectangle clientArea = rootWidget.getClientArea ();
-		tabFolder.setLocation (clientArea.x, clientArea.y);
+	  	
+	  	// dont know what these do:
+		// Rectangle clientArea = rootWidget.getClientArea ();
+		// tabFolder.setLocation (clientArea.x, clientArea.y);
 		
 		TabItem item = new TabItem (tabFolder, SWT.NONE);
 		item.setText ("Classes");
-		Composite panel = new Composite(tabFolder,SWT.NONE);
-		panel.setLayout(new GridLayout());
+		
+		final ScrolledComposite scr = new ScrolledComposite(tabFolder, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+		// scr.setLayout(new GridLayout()); --- not required
+		final Composite panel = new Composite(scr,SWT.NONE);
+		scr.setContent(panel);
+
+		// panel.setSize(600,200);
+
+		
+		GridLayout layout = new GridLayout();
+		panel.setLayout(layout);
+		
 		for (int i=0; i<6; i++) {
 		Button button = new Button (panel, SWT.PUSH);
 		button.setText ("Strings");
+		button.setSize(600,600);
 		}
-		item.setControl (panel);
 		
-		
+		item.setControl (scr);
+		// scr.pack(); // --- not required
+		panel.pack();
 		tabFolder.pack ();
 		rootWidget.pack ();
 		rootWidget.open ();		      
