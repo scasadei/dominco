0c1ff8199e5b2e0654ee9e9bb0e69d04bf0c06f2
stefano casadei <scasadei@yahoo.com>
2011-09-14 14:46:20 +0200

    fixed DomainContext; no more errors in the project!

--- core/src/net/efano/dominco/DomainContext.java
+++ core/src/net/efano/dominco/DomainContext.java
@@ -2,11 +2,9 @@ package net.efano.dominco;
 
 import java.util.Properties;
 
-import org.eclipse.swt.widgets.Table;
-
 import net.efano.dominco.ui.IViewable;
+import net.efano.dominco.ui.IViewpage;
 import net.efano.dominco.ui.IViewpageFactory;
-import net.efano.dominco.ui.swt.DomainContextViewAsTable;
 
 public class DomainContext implements IViewable {
 
@@ -14,7 +12,7 @@ public class DomainContext implements IViewable {
 	/*
 	 * An optional gui element to show itself
 	 */
-	private DomainContextViewAsTable viewpage;
+	private IViewpage viewpage;
 	
 	
 	@Override
@@ -28,14 +26,11 @@ public class DomainContext implements IViewable {
 			try {Thread.sleep(100);}
 			catch (InterruptedException e) {}
 		}
-
 		// syncExec waits that argument terminates
-    	// df.getDisplay().syncExec(new Runnable() {
         df.syncExec(new Runnable() {
 			public void run () {
-				Table swtTable = df.getNewViewpage("DomContext",
+				viewpage = df.getNewViewpage("DomContext",
 						new String []{"a","b"});
-				viewpage = new DomainContextViewAsTable(swtTable);
 				viewpage.setViewable(DomainContext.this);
 			}
 		});
